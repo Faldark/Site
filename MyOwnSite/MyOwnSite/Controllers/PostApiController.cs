aefba2a1ec21ac5a06e02d90433e1d32f69611db
BChukhno <bogdan.chukhno@gmail.com>
2018-03-31 23:17:31 +0300

    9

--- MyOwnSite/MyOwnSite/Controllers/PostApiController.cs
+++ MyOwnSite/MyOwnSite/Controllers/PostApiController.cs
@@ -20,7 +20,7 @@ namespace MyOwnSite.Controllers
     public class PostApiController : ApiController
     {
         
-        //18
+        //189
         [Dependency]
 
         public IPostService PostService { get; set; }
